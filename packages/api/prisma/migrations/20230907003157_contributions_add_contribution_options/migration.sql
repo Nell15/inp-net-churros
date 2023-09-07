5c9660807ac95b29609d2d9c11c1f842d5bc5e8e
Ewen Le Bihan <hey@ewen.works>
2023-09-07 03:15:18 +0200

    feat(contributions): implement contribution options

--- packages/api/prisma/migrations/20230907003157_contributions_add_contribution_options/migration.sql
+++ packages/api/prisma/migrations/20230907003157_contributions_add_contribution_options/migration.sql
@@ -2,12 +2,16 @@
   Warnings:
 
   - You are about to drop the column `studentAssociationId` on the `Contribution` table. All the data in the column will be lost.
+  - You are about to drop the column `contributionPrice` on the `StudentAssociation` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[optionId,userId]` on the table `Contribution` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `optionId` to the `Contribution` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropForeignKey
 ALTER TABLE "Contribution" DROP CONSTRAINT "Contribution_studentAssociationId_fkey";
 
 -- DropIndex
+DROP INDEX "Contribution_userId_studentAssociationId_key";
 
 -- AlterTable
 ALTER TABLE "Announcement" ALTER COLUMN "id" SET DEFAULT nanoid('ann:');
@@ -19,7 +23,8 @@ ALTER TABLE "Article" ALTER COLUMN "id" SET DEFAULT nanoid('a:');
 ALTER TABLE "BarWeek" ALTER COLUMN "id" SET DEFAULT nanoid('barweek:');
 
 -- AlterTable
-ALTER TABLE "Contribution"
+ALTER TABLE "Contribution" DROP COLUMN "studentAssociationId",
+ADD COLUMN     "optionId" TEXT NOT NULL,
 ALTER COLUMN "id" SET DEFAULT nanoid('contribution:');
 
 -- AlterTable
@@ -71,7 +76,8 @@ ALTER TABLE "Registration" ALTER COLUMN "id" SET DEFAULT nanoid('r:');
 ALTER TABLE "School" ALTER COLUMN "id" SET DEFAULT nanoid('school:');
 
 -- AlterTable
-ALTER TABLE "StudentAssociation" ALTER COLUMN "id" SET DEFAULT nanoid('ae:');
+ALTER TABLE "StudentAssociation" DROP COLUMN "contributionPrice",
+ALTER COLUMN "id" SET DEFAULT nanoid('ae:');
 
 -- AlterTable
 ALTER TABLE "Ticket" ALTER COLUMN "id" SET DEFAULT nanoid('t:');
@@ -86,28 +92,42 @@ ALTER TABLE "User" ALTER COLUMN "id" SET DEFAULT nanoid('u:');
 ALTER TABLE "UserCandidate" ALTER COLUMN "id" SET DEFAULT nanoid('candidate:');
 
 -- CreateTable
-CREATE TABLE "_ContributionToStudentAssociation" (
+CREATE TABLE "ContributionOption" (
+    "id" TEXT NOT NULL DEFAULT nanoid('contributionoption:'),
+    "offeredInId" TEXT NOT NULL,
+    "lydiaAccountId" TEXT,
+    "name" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "ContributionOption_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ContributionOptionToStudentAssociation" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "_ContributionToStudentAssociation_AB_unique" ON "_ContributionToStudentAssociation"("A", "B");
+CREATE UNIQUE INDEX "_ContributionOptionToStudentAssociation_AB_unique" ON "_ContributionOptionToStudentAssociation"("A", "B");
 
 -- CreateIndex
-CREATE INDEX "_ContributionToStudentAssociation_B_index" ON "_ContributionToStudentAssociation"("B");
+CREATE INDEX "_ContributionOptionToStudentAssociation_B_index" ON "_ContributionOptionToStudentAssociation"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Contribution_optionId_userId_key" ON "Contribution"("optionId", "userId");
 
 -- AddForeignKey
-ALTER TABLE "_ContributionToStudentAssociation" ADD CONSTRAINT "_ContributionToStudentAssociation_A_fkey" FOREIGN KEY ("A") REFERENCES "Contribution"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Contribution" ADD CONSTRAINT "Contribution_optionId_fkey" FOREIGN KEY ("optionId") REFERENCES "ContributionOption"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_ContributionToStudentAssociation" ADD CONSTRAINT "_ContributionToStudentAssociation_B_fkey" FOREIGN KEY ("B") REFERENCES "StudentAssociation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "ContributionOption" ADD CONSTRAINT "ContributionOption_offeredInId_fkey" FOREIGN KEY ("offeredInId") REFERENCES "School"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
--- Migrate foreign keys
---- Insert values into _ContributionToStudentAssociation, setting "A" to the contribution id and "B" to the studentAssociationId; reading from the Contributions table
-INSERT INTO "_ContributionToStudentAssociation" ("A", "B")
-SELECT "id", "studentAssociationId"
-FROM "Contribution";
+-- AddForeignKey
+ALTER TABLE "ContributionOption" ADD CONSTRAINT "ContributionOption_lydiaAccountId_fkey" FOREIGN KEY ("lydiaAccountId") REFERENCES "LydiaAccount"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
-DROP INDEX "Contribution_userId_studentAssociationId_key";
-ALTER TABLE "Contribution" DROP COLUMN "studentAssociationId";
+-- AddForeignKey
+ALTER TABLE "_ContributionOptionToStudentAssociation" ADD CONSTRAINT "_ContributionOptionToStudentAssociation_A_fkey" FOREIGN KEY ("A") REFERENCES "ContributionOption"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ContributionOptionToStudentAssociation" ADD CONSTRAINT "_ContributionOptionToStudentAssociation_B_fkey" FOREIGN KEY ("B") REFERENCES "StudentAssociation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
