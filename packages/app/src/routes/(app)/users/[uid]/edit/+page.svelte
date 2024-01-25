329939ff39c4b08ac492d5e34e697e33710d2754
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-01-25 13:58:20 +0100

    WIP: continue

--- packages/app/src/routes/(app)/users/[uid]/edit/+page.svelte
+++ packages/app/src/routes/(app)/users/[uid]/edit/+page.svelte
@@ -36,7 +36,6 @@
 
   const deleteToken = async (id: string, active: boolean) => {
     if (active) {
-      window.localStorage.setItem('isReallyLoggedout', 'true');
       await goto(`/logout/?token=${$page.data.token!}`);
     } else {
       await $zeus.mutate({ deleteToken: [{ id }, true] });
