609af31c2b24363230db5611ad2c2bf33ea765cb
Benjamin SOYER <benjamin.soyer27@gmail.com>
2024-01-31 17:39:31 +0100

    feat(seeding): group generation, event and tickets (j'ai rien testé je suis terrifié)

--- packages/api/src/seedFaker.ts
+++ packages/api/src/seedFaker.ts
@@ -424,11 +424,10 @@ for (const[_, data] of usersData.entries()){
 }
 
 const users = await prisma.user.findMany();
-const userEEEA = users.filter((element) => element.majorId === "eaaa");
 
 let numberSubject : number = 10; 
 //creation de nbSubject pour toute les mineurs des filières possible
-for (const[i, minor] of minors.entries()){
+for (const[_, minor] of minors.entries()){
   for(let j=0; j< numberSubject; j++){
       await prisma.document.create({
         data:{
@@ -436,7 +435,7 @@ for (const[i, minor] of minors.entries()){
           title: 'Un document',
           uid: 'un-document',
           schoolYear: faker.number.int({ min : 2015, max : 2024}),
-          subject: { connect: { id: minors[i]?.id } }, 
+          subject: { connect: { id: minor?.id } }, 
           type: 'Exam',
           uploader: { connect: { uid: faker.helpers.arrayElement(users.filter((element) => element.minorId === minor.id)).uid } }, //recup uniquement les users de la bonne mineur pour en faire des auteurs
         }
@@ -444,14 +443,429 @@ for (const[i, minor] of minors.entries()){
     }
 }
 
+const clubsData = [
+  { name: 'Art' },
+  { name: 'Basket' },
+  { name: 'Cinéma' },
+  { name: 'Danse' },
+  { name: 'Escalade' },
+  { name: 'Football' },
+  { name: 'Golf' },
+  { name: 'Handball' },
+  { name: 'Igloo' },
+  { name: 'Jardinage' },
+  { name: 'Karaté' },
+  { name: 'Lecture' },
+  { name: 'Musique' },
+  { name: 'Natation' },
+  { name: 'Origami' },
+  { name: 'Pétanque' },
+  { name: 'Quidditch' },
+  { name: 'Randonnée' },
+  { name: 'Ski' },
+  { name: 'Tennis' },
+  { name: 'Ukulélé' },
+  { name: 'Vélo' },
+  { name: 'Water-polo' },
+  { name: 'Xylophone' },
+  { name: 'Yoga' },
+  { name: 'Zumba' },
+];
 
-interface subject{
+for (const [_, group] of clubsData.entries()) {
+  const { id: groupId } = await prisma.group.create({
+    data: {
+      ...group,
+      uid: slug(group.name),
+      type: GroupType.Club,
+      color: color(group.name),
+      address: 'D202',
+      email: `${slug(group.name)}@list.example.com`,
+      website: `https://${slug(group.name)}.example.com`,
+      description: `Club ${group.name} de l'école`,
+      longDescription: `# Caeco ambrosia defendite simplicitas aequore caelestibus auro
 
+      Lorem markdownum accessit desperat lumina; hi sed radice Scylla agger. Et ipsa
+      cum **Tereus**, aequore sedet. [Quem qua](/) qui carmine,
+      ore suus, fixa natus lacrimas.
+
+      Perque dederat bracchia tenui Leucothoe in in sequitur fames non hic. Venitque
+      sua anguem [sed](/) supponere sit, fluctus pedibusque ne apros
+      rotis exauditi mater voluistis carinam habet generosam miserrima. Quoquam
+      ulterius quam; pressit mihi germanae faciemque: in certa cruor solacia est caeli
+      suos auras atra!
+
+      > Explorant est illi inhaesuro doloris sed *inmanis* has recessu, quam interdum
+      > hospes. Et huc postquam subdit incertas: echidnae, o cibique spectat sed
+      > diversa. Placuit omnia; flammas Hoc ventis nobis primordia flammis Mavors
+      > dabat horrida conplecti cremantur. A mundus, metu Anius gestare caelatus,
+      > Alpheos est, lecti et?`,
+      studentAssociation: { connect: { id: faker.helpers.arrayElement(studentAssociations).id } },
+      links: {
+        createMany: {
+          data: [
+            { name: 'Facebook', value: '#' },
+            { name: 'Instagram', value: '#' },
+          ],
+        },
+      },
+    },
+  });
+  await prisma.group.update({
+    where: { id: groupId },
+    data: {
+      familyRoot: { connect: { id: groupId } },
+    },
+  });
 }
 
-/*function createSubject(): subject{ 
-    return{
-        firstName: faker.person.firstName(),
-        lastName: faker.person.firstName()
-    };
-}*/
+let Intégration2022 = await prisma.group.create({
+  data: {
+    name: 'Intégration 2022',
+    type: GroupType.Group,
+    uid: 'integration-2022',
+    color: '#ff0000',
+    links: { create: [] },
+  },
+});
+
+Intégration2022 = await prisma.group.update({
+  where: { id: Intégration2022.id },
+  data: {
+    familyRoot: { connect: { id: Intégration2022.id } },
+  },
+});
+
+const Groupe1 = await prisma.group.create({
+  data: {
+    name: 'Groupe 1',
+    type: GroupType.Integration,
+    uid: 'groupe-1',
+    color: '#00ff00',
+    parent: { connect: { id: Intégration2022.id } },
+    familyRoot: { connect: { id: Intégration2022.familyId! } },
+    links: { create: [] },
+    // members: { createMany: { data: [{ memberId: 2 }, { memberId: 3 }, { memberId: 4 }] } },
+  },
+});
+
+await prisma.group.create({
+  data: {
+    name: 'Groupe 2',
+    type: GroupType.Integration,
+    uid: 'groupe-2',
+    color: '#0000ff',
+    parent: { connect: { id: Intégration2022.id } },
+    familyRoot: { connect: { id: Intégration2022.familyId! } },
+    links: { create: [] },
+    // members: { createMany: { data: [{ memberId: 5 }, { memberId: 6 }, { memberId: 7 }] } },
+  },
+});
+
+let groups = await prisma.group.findMany({ include: { members: { include: { member: true } } } });
+
+const groupMembers: Prisma.GroupMemberCreateManyInput[] = [];
+
+for (const group of groups) {
+  const randomUserIDs = faker.helpers.arrayElements(users, { min : 10, max: 30});
+  groupMembers.push(
+    {
+      groupId: group.id,
+      memberId: randomUserIDs[0]!.id,
+      title: 'Prez',
+      president: true,
+      canEditArticles: true,
+      canEditMembers: true,
+    },
+    {
+      groupId: group.id,
+      memberId: randomUserIDs[1]!.id,
+      title: 'Trez',
+      vicePresident: true,
+      canEditArticles: true,
+      canEditMembers: true,
+    },
+    {
+      groupId: group.id,
+      memberId: randomUserIDs[2]!.id,
+      title: 'VP',
+      treasurer: true,
+      canEditArticles: true,
+      canEditMembers: true,
+    },
+    {
+      groupId: group.id,
+      memberId: randomUserIDs[3]!.id,
+      title: 'Secrétaire',
+      canEditArticles: true,
+      canEditMembers: true,
+    },
+    {
+      groupId: group.id,
+      memberId: randomUserIDs[4]!.id,
+      title: 'Respo Com',
+      canEditArticles: true,
+    }, //on peut continuer a souhait pour créer d'autres membres avec des roles spéciaux si besoin...
+    {
+      groupId: group.id,
+      memberId: randomUserIDs[4]!.id,
+    }
+  );
+  for(let i=5; i < randomUserIDs.length; i++){
+    groupMembers.push(
+      {
+        groupId: group.id,
+        memberId: randomUserIDs[i]!.id
+      }
+    )
+  }
+}
+
+await prisma.groupMember.createMany({ data: groupMembers });
+
+groups = await prisma.group.findMany({ include: { members: { include: { member: true } } } });
+
+const articleData: Prisma.ArticleCreateInput[] = [];
+
+
+const end = 26 * 5;
+const currentDate = Date.now()
+const startDate = new Date(faker.date.anytime({ refDate: currentDate})).getTime(); //génération d'une date autour de la date actuelle=date où tu build
+const endDate = new Date(faker.date.future({ refDate : startDate })).getTime();
+
+for (let i = 0; i < end; i++) {
+  const group = faker.helpers.arrayElement(groups);
+  articleData.push({
+    uid: `article-${i}`,
+    title: `Article ${i}`,
+    group: {
+      connect: {
+        id: group.id,
+      },
+    },
+    author:
+      i % 11 === 0
+        ? undefined
+        : {
+            connect: { id: faker.helpers.arrayElement(group.members.map((m) => m.member)).id },
+          },
+    body: `**Lorem ipsum dolor sit amet**, consectetur adipiscing elit. Ut feugiat velit sit amet tincidunt gravida. Duis eget laoreet sapien, id.
+
+[Lorem ipsum dolor.](/)
+
+# Partie 1
+
+1. Un
+2. Deux
+3. Trois
+
+# Partie 2
+
+- Un
+- Deux
+- Trois
+`,
+    visibility: i % 3 === 0 ? Visibility.Public : Visibility.GroupRestricted,
+    published: i % 7 > 1,
+    createdAt: new Date(startDate * (1 - i / end) + endDate * (i / end)),
+    publishedAt: new Date( //ce beau bordel je l'édit avec faker ???
+      startDate * (1 - i / end) + endDate * (i / end) + (i % 7) * 24 * 60 * 60 * 1000,
+    ),
+    links: {
+      create: [
+        {
+          name: 'Facebook',
+          value: 'https://facebook.com',
+        },
+        {
+          name: 'Trop cool',
+          value: 'https://youtu.be/dQw4w9WgXcQ',
+        },
+      ],
+    },
+  });
+}
+
+for (const data of articleData) await prisma.article.create({ data });
+
+await prisma.article.create({
+  data: {
+    title: "C'est le début de l'inté",
+    body: `début de l'inté, vous allez devenir alcoolique et vomir partout`,
+    uid: 'cest-le-debut-de-l-inte',
+    group: {
+      connect: { id: Intégration2022.id },
+    },
+    published: true,
+    links: {
+      create: [],
+    },
+  },
+});
+
+for(let i =0; i < 5; i++){
+  let selectedClub = faker.helpers.arrayElement(groups);
+  let capacityEvent = faker.number.int({min: 30, max: 300});
+  await prisma.event.create({
+    data:{
+      contactMail: 'hey@ewen.works',
+      description: 'Ceci est un événement',
+      endsAt: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
+      startsAt: new Date(),
+      uid: 'ceci-est-un-evenement',
+      title: 'Ceci est un événement',
+      group: { connect: { id: selectedClub.id } },
+      visibility: Visibility.Public,
+      articles: {
+        createMany: {
+          data: [
+            {
+              body: "Ceci est un article d'événement",
+              groupId: selectedClub.id,
+              uid: 'ceci-est-un-article-d-evenement',
+              title: "Ceci est un article d'événement",
+            },
+          ],
+        },
+      },
+      links: {
+        createMany: {
+          data: [
+            {
+              name: 'Facebook',
+              value: 'https://facebook.com',
+            },
+            {
+              name: 'Trop cool',
+              value: 'https://youtu.be/dQw4w9WgXcQ',
+            },
+          ],
+        },
+      },
+      tickets: {
+        createMany: {
+          data: [
+            {
+              uid: `event-${selectedClub.uid}`,
+              name: `Event ${selectedClub.name}`,
+              description: 'blablabla ramenez vos culs par pitié je vous en supplie',
+              price: faker.number.int({min : 0, max: 30}),
+              capacity: capacityEvent,
+              opensAt: new Date(),
+              closesAt: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
+              allowedPaymentMethods: ['Cash', 'Lydia', 'Card'],
+              openToPromotions: [2024, 2025, 2026],
+              openToAlumni: faker.datatype.boolean(),
+              openToExternal: faker.datatype.boolean(),
+              openToContributors: faker.datatype.boolean(),
+              godsonLimit: 0,
+              onlyManagersCanProvide: faker.datatype.boolean(),
+            },
+          ],
+        },
+      },
+    },
+    include: {
+      tickets: true,
+    },
+  })
+}
+
+let events = await prisma.event.findMany({ include: { tickets: true } });
+let selectedEvent  = faker.helpers.arrayElement(events);
+
+const registration = await prisma.registration.create({
+  data: {
+    ticketId: faker.helpers.arrayElement(selectedEvent.tickets).id,
+    authorId: faker.helpers.arrayElement(users).id,
+    paymentMethod: 'Lydia',
+    paid: false,
+    beneficiary: 'annie',
+  },
+});
+
+for (const i of range(0, 100)) {
+  selectedEvent  = faker.helpers.arrayElement(events);
+  await prisma.registration.create({
+    data: {
+      createdAt: randomTime(registration.createdAt, range(13, 23)),
+      ticketId: faker.helpers.arrayElement(selectedEvent.tickets).id,
+      authorId:
+        // eslint-disable-next-line unicorn/no-null
+        i % 4 === 0 ? null : faker.helpers.arrayElement(users.filter((u) => u.id !== registration.authorId)).id, //c'est quoi l'objectif ? uwu :3
+      authorEmail: i % 4 === 0 ? 'feur@quoi.com' : undefined,
+      paymentMethod: i % 2 === 0 ? 'Lydia' : 'Cash',
+      paid: true,
+      beneficiary: i % 4 === 0 ? 'whatcoubeh' : faker.helpers.arrayElement(users).uid,
+    },
+  });
+}
+
+
+await prisma.ticket.update({
+  where: { id: events[0]!.tickets[0]!.id },
+  data: {
+    openToGroups: {
+      connect: [{ uid: 'ski' }],
+    },
+    links: {
+      createMany: {
+        data: [
+          {
+            name: "C'est le menu",
+            value: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+          },
+        ],
+      },
+    },
+  },
+});
+
+for(const[_, data] of groups.entries()){
+  await prisma.event.create({
+    data: {
+      contactMail: `${data.name}@chipichipichapachpamail.com`,
+      description: 'Viens passer la passation TVn7 avec nous !',
+      endsAt: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
+      startsAt: new Date(),
+      uid: 'passation-tvn7',
+      title: 'Passation TVn7',
+      visibility: Visibility.GroupRestricted,
+      author: { connect: { uid: 'deuxtroisq' } },
+      group: { connect: { uid: 'ski' } },
+      links: {
+        createMany: {
+          data: [
+            {
+              name: 'Menu',
+              value: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+            },
+          ],
+        },
+      },
+      tickets: {
+        createMany: {
+          data: [
+            {
+              name: '',
+              description: '',
+              uid: 'ticket',
+              price: 3.5,
+              capacity: 70,
+              allowedPaymentMethods: ['Lydia'],
+              closesAt: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
+              opensAt: new Date(),
+              godsonLimit: 0,
+              // eslint-disable-next-line unicorn/no-null
+              openToAlumni: null,
+              openToExternal: false,
+              // eslint-disable-next-line unicorn/no-null
+              openToContributors: null,
+              openToPromotions: [],
+            },
+          ],
+        },
+      },
+    },
+  });
+} //en vrai ça on peut l'exploser ??
